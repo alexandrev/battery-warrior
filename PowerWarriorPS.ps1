2fae6c524f8703a356df0518de52a053a0a7da6f
Alexandre Vazquez <alexandre.vazquez@gmail.com>
2016-03-06 19:26:06 +0100

    Adding a System Tray Icon and Bubble notifications

--- PowerWarriorPS.ps1
+++ PowerWarriorPS.ps1
@@ -9,6 +9,28 @@ Import-Module PSLogging
 Start-Log -LogPath $logPath -LogName "battery-warrior.log" -ScriptVersion "1.0" | out-null
 $fullLogPath = $logPath + "/battery-warrior.log" 
 
+Function loadIcon {
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+    $objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
+
+    $objNotifyIcon.Icon = "D:\Projects\GitHub\battery-warrior\resources\icon.ico"
+    return $objNotifyIcon;  
+}
+
+Function showMessage {    
+    Param(
+    $title = "Battery Warrior",
+    $text = "Sample Text",  
+    $obj = $null     
+    )
+
+    $obj.BalloonTipIcon = "Info" 
+    $obj.BalloonTipText = $text
+    $obj.BalloonTipTitle = $title
+    $obj.Visible = $True 
+    $obj.ShowBalloonTip(10000)
+}
+
 Function isOnAC
 {
 Param(
@@ -59,31 +81,39 @@ finally {
 
 
 Write-LogInfo -LogPath $fullLogPath -Message "Starting the execution of the BatteryWarrior." -TimeStamp
+$obj = loadIcon
 $WorkingFlag = $true
 $CurrentMode = "-1"
-while ($WorkingFlag -eq $true){    
-    Write-LogInfo -LogPath $fullLogPath -Message  "Checking for a change of state of the laptop"  -TimeStamp   
-    $isPowered = (isOnAC -computer $computer)
-    Write-LogInfo -LogPath $fullLogPath -Message  "The laptop is currently under AC power: $isPowered" -TimeStamp
-    if($isPowered -eq $true){
-        Write-LogInfo -LogPath $fullLogPath -Message  "Analyzing the $CurrentMode and comparing to the laptop status" -TimeStamp
-        if(-not ($CurrentMode -eq "1")) {
-            Write-LogInfo -LogPath $fullLogPath -Message  "Launching High Power Consuming Apps" -TimeStamp
-            managingApps -mode "1" -filePath $appData
-            $CurrentMode = "1"
+try{
+    while ($WorkingFlag -eq $true){    
+        Write-LogInfo -LogPath $fullLogPath -Message  "Checking for a change of state of the laptop"  -TimeStamp   
+        $isPowered = (isOnAC -computer $computer)
+        Write-LogInfo -LogPath $fullLogPath -Message  "The laptop is currently under AC power: $isPowered" -TimeStamp
+        if($isPowered -eq $true){
+            Write-LogInfo -LogPath $fullLogPath -Message  "Analyzing the $CurrentMode and comparing to the laptop status" -TimeStamp
+            if(-not ($CurrentMode -eq "1")) {
+                showMessage -obj $obj -title "Battery Warrior" -text "Launching High Power Consuming Apps" 
+                Write-LogInfo -LogPath $fullLogPath -Message  "Launching High Power Consuming Apps" -TimeStamp
+                managingApps -mode "1" -filePath $appData
+                $CurrentMode = "1"
+            }
+        }else{
+            Write-LogInfo -LogPath $fullLogPath -Message  "2 Analyzing the $CurrentMode and comparing to the laptop status" -TimeStamp
+            if(-not ($CurrentMode -eq "0")) {
+                showMessage -obj $obj -title "Battery Warrior" -text "Killing High Power Consuming Apps"
+                Write-LogInfo -LogPath $fullLogPath -Message  "Killing High Power Consuming Apps" -TimeStamp
+                managingApps -mode "0" -filePath $appData
+                $CurrentMode = "0"
+            }
         }
-    }else{
-        Write-LogInfo -LogPath $fullLogPath -Message  "2 Analyzing the $CurrentMode and comparing to the laptop status" -TimeStamp
-        if(-not ($CurrentMode -eq "0")) {
-            Write-LogInfo -LogPath $fullLogPath -Message  "Killing High Power Consuming Apps" -TimeStamp
-            managingApps -mode "0" -filePath $appData
-            $CurrentMode = "0"
-        }
+        
+        Write-LogInfo -LogPath $fullLogPath -Message  "Current Mode: $CurrentMode" -TimeStamp
+        Write-LogInfo -LogPath $fullLogPath -Message  "Sleeping for $sleepTime seconds" -TimeStamp
+        sleep $sleepTime    
     }
-    
-    Write-LogInfo -LogPath $fullLogPath -Message  "Current Mode: $CurrentMode" -TimeStamp
-    Write-LogInfo -LogPath $fullLogPath -Message  "Sleeping for $leepTime seconds" -TimeStamp
-    sleep $sleepTime    
-}
 
-Start-Log -LogPath $fullLogPath
+}finally
+{
+    $obj.Dispose()
+    Stop-Log -LogPath $fullLogPath
+}
